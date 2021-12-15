4e3a1faccb65bfaf885697f904463dfd48aed4e6
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-15 17:41:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,4 +2,4 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      BOT_TOKEN = os.environ.get("2021474319:AAFQooY34WUZzyYqryxYMGsQd4wB7oP9Azg", "2021474319:AAFQooY34WUZzyYqryxYMGsQd4wB7oP9Azg")
